d2265f222719a493682f11b65514e6d37e3721ff
Funny-Bunny <work.nyu@gmail.com>
2023-03-13 01:06:41 +0300

    Add changes to unit 4

--- Seminar_001/Unit_004/Program.cs
+++ Seminar_001/Unit_004/Program.cs
@@ -1,9 +1,8 @@
 ﻿//Напишите программу, которая принимает на вход трёхзначное число 
 //и на выходе показывает последнюю цифру этого числа.
 
+
 Console.Clear();
 Console.Write("Введите трехзначное число: ");
-int num=int.Parse(Console.ReadLine());
-int y=num%10;
-
-Console.WriteLine($"{y}");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(number % 10);
