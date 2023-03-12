aaaf72c367a71d8c281b0191af49a54433265508
Funny-Bunny <work.nyu@gmail.com>
2023-03-13 01:15:14 +0300

    Add changes to unit 3

--- Seminar_001/Unit_003/Program.cs
+++ Seminar_001/Unit_003/Program.cs
@@ -1,10 +1,12 @@
 ﻿//Напишите программу, которая на вход принимает одно число (N), 
 //а на выходе показывает все целые числа в промежутке от -N до N.
 
-int N = int.Parse(Console.ReadLine());
-int B = -N;
-while (B <= N)
+
+Console.Clear();
+Console.Write("Введите число: ");
+int numberUser = int.Parse(Console.ReadLine());
+for (int i = -numberUser; i <= numberUser; i++)
 {
-    Console.WriteLine($"{B} ");
-    B++;
+    Console.Write(i + ", ");
 }
+Console.WriteLine();
