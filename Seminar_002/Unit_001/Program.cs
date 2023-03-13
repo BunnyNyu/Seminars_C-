1d10450508aaa84f236d2ce5a5f839bfecceb8bd
Funny-Bunny <work.nyu@gmail.com>
2023-03-14 01:24:36 +0300

    Add new project Seminar 002 with folder Unit 001

---
+++ Seminar_002/Unit_001/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу, которая выводит случайное число отрезка [10, 99] и показывает наибольшую цифру этого числа
+// 78 -> 8
+
+
+Console.Clear();
+
+int num = new Random().Next(10, 100);
+Console.WriteLine($"Число: {num}");
+
+int firstValue = num / 10; //определяем первое число путем целочисленного деления
+int secondValue = num % 10; //опеределяем второе число путем деления на остаток
+
+int max = firstValue;
+if (secondValue > max)
+{
+    max = secondValue;
+}
+
+Console.WriteLine($"Максимальная цифра в числе {max}");
