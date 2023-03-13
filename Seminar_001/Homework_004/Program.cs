4a1fafa1dbc6578790430b43f1f24fd3e5eb6405
Funny-Bunny <work.nyu@gmail.com>
2023-03-13 16:39:29 +0300

    Add info to homework 4

--- Seminar_001/Homework_004/Program.cs
+++ Seminar_001/Homework_004/Program.cs
@@ -2,3 +2,12 @@
 // , а на выходе показывает все чётные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= number; i = i + 2)
+{
+    Console.Write(i + ", ");
+}
+Console.WriteLine();
