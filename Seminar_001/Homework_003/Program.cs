669e368e524d77b837ac311e3e3a5b9327829e20
Funny-Bunny <work.nyu@gmail.com>
2023-03-13 16:26:24 +0300

    Add info to homework 3

--- Seminar_001/Homework_003/Program.cs
+++ Seminar_001/Homework_003/Program.cs
@@ -3,3 +3,15 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine(number + " является чётным числом");
+}
+else
+{
+    Console.WriteLine(number + " не является чётным числом");
+}
