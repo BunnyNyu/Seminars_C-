ab493f5be434b9fee726527ab6439163d103df9e
Funny-Bunny <work.nyu@gmail.com>
2023-03-14 17:05:12 +0300

    Add changes to Unit 002

--- Seminar_002/Unit_002/Program.cs
+++ Seminar_002/Unit_002/Program.cs
@@ -8,8 +8,12 @@ Console.Clear();
 int num = new Random().Next(100, 1000);
 Console.Write($"Число: {num}");
 
-int firstValue = num / 100; //определяем первое число путем целочисленного деления
-int secondValue = num % 10; //опеределяем третье число число путем деления на остаток
+int firstNum = num / 100; //определяем первое число путем целочисленного деления
+int secondNum = num % 10; //опеределяем третье число число путем деления на остаток
 
-int result = firstValue * 10 + secondValue;
-Console.Write($" ==>> {result}");
+//int result = firstNum * 10 + secondNum;
+//Console.Write($" ==>> {result}");
+
+//или
+
+Console.Write($" ==>> {firstNum}{secondNum}");
