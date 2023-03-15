885dd2975f65967f9cf9e0a756b24523c8db9af4
Funny-Bunny <work.nyu@gmail.com>
2023-03-15 18:36:34 +0300

    Add cnanges 4 to homework 002

--- Seminar_002/Homework_002/Program.cs
+++ Seminar_002/Homework_002/Program.cs
@@ -4,49 +4,27 @@
 // 7 -> да
 // 1 -> нет
 
-//индексы        0  1  2  3  4
-int[] array1 = { 1, 2, 3, 4, 5 };
-int[] array2 = { 6, 7 };
-int n1 = array1.Length;
-int n2 = array2.Length;
+//индексы       0  1  2  3  4  5  6
+int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+int n = array.Length;
 int find = new Random().Next(1, 8);
 Console.Write($"День недели номер {find} выходной?: ");
 
-int index1 = 0;
-int index2 = 0;
+int index = 0;
 
-while (index1 < n1 && index2 < n2)
+while (index < n)
 {
-    if (array1[index1] == find)
+    int result = array[index];
+    if (array[index] == find)
     {
-        int result1 = index1;
-        Console.Write(result1);
+        if (result < 6)
+        {
+            Console.Write("net");
+        }
+        else
+        {
+            Console.Write("da");  
+        }
     }
-    else if (array2[index2] == find);
-    {
-        int result2 = index2;
-        Console.Write(result2);
-    }
- index1++;
- index2++;
+index++;
 }
-
-
-// где-то ошибка...
-//индексы       0   1   2  3   4   5   6   7   8
-//int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 }; //если имеется два одинаковых искомых элемента
-                                                    //то в результате программа покажет оба 
-//int n = array.Length;
-//int find = 18;
-
-//int index = 0;
-
-//while (index < n)
-//{
-//    if(array[index] == find)
-//    {
-//        Console.WriteLine(index);
-//        break;  //останавливает процесс поиска при первом найденном соответствии
-//    }
-//    index++;    //index = index + 1;
-//}
