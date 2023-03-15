a5ca815365081929cfbd18cee614a15314813a33
Funny-Bunny <work.nyu@gmail.com>
2023-03-15 17:48:04 +0300

    Add changes 3 to homework 002

--- Seminar_002/Homework_002/Program.cs
+++ Seminar_002/Homework_002/Program.cs
@@ -15,22 +15,38 @@ Console.Write($"День недели номер {find} выходной?: ");
 int index1 = 0;
 int index2 = 0;
 
-while (index1 < n1 || index2 < n2)
+while (index1 < n1 && index2 < n2)
 {
     if (array1[index1] == find)
     {
         int result1 = index1;
-        Console.WriteLine(result1);
-        index1++;
-        break;
+        Console.Write(result1);
     }
-    else if (array2[index2] == find) ;
+    else if (array2[index2] == find);
     {
         int result2 = index2;
-        Console.WriteLine(result2);
-        index2++;
+        Console.Write(result2);
     }
+ index1++;
+ index2++;
 }
 
 
 // где-то ошибка...
+//индексы       0   1   2  3   4   5   6   7   8
+//int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 }; //если имеется два одинаковых искомых элемента
+                                                    //то в результате программа покажет оба 
+//int n = array.Length;
+//int find = 18;
+
+//int index = 0;
+
+//while (index < n)
+//{
+//    if(array[index] == find)
+//    {
+//        Console.WriteLine(index);
+//        break;  //останавливает процесс поиска при первом найденном соответствии
+//    }
+//    index++;    //index = index + 1;
+//}
