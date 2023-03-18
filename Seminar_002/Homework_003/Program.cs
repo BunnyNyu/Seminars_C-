95b4d98e560aaef120829a9d61812679044f46a0
Funny-Bunny <work.nyu@gmail.com>
2023-03-18 18:26:21 +0300

    Add finish changes to homework 003

--- Seminar_002/Homework_003/Program.cs
+++ Seminar_002/Homework_003/Program.cs
@@ -5,13 +5,21 @@
 // 32679 -> 6
 
 Console.Clear();
-
 Console.Write("Введите число: ");
 int userNum = Convert.ToInt32(Console.ReadLine());
 
-while (userNum <= 999)
+while (userNum > 999)
 {
-    int resultNum = userNum / 10;
+    int firstNum = userNum / 10;
+    userNum = firstNum;
 }
-int result = resultNum;
-Console.WriteLine(resultNum);
+if (userNum > 99)
+{
+    int secondNum = userNum % 10;
+    Console.Write(secondNum);
+}
+else
+{
+    Console.Write("Третьего числа нет");
+}
+
