6e5bd4414241449d82c35bc3d214bfb76cc33d40
Funny-Bunny <work.nyu@gmail.com>
2023-03-19 14:46:54 +0300

    Add changes to homework 002

--- Seminar_002/Homework_002/Program.cs
+++ Seminar_002/Homework_002/Program.cs
@@ -4,6 +4,37 @@
 // 7 -> да
 // 1 -> нет
 
+
+
+
+
+// Вариант 1
+Console.Write("Введите день недели: ");
+int numberDayOfWeek = int.Parse(Console.ReadLine());
+
+if (numberDayOfWeek == 6 || numberDayOfWeek == 7)
+{
+    Console.WriteLine("Да, это выходной");
+
+}
+else
+{
+    Console.WriteLine("Нет, это не выходной");
+}
+
+
+
+
+
+// Вариант 2 - тернарная операция - "?" заменяется if, ":" заменяет else
+string message = numberDayOfWeek == 6 || numberDayOfWeek == 7 ? "Да, это выходной" : "Нет, это не выходной";
+Console.WriteLine (message);
+
+
+
+
+
+// Вариант 3 (массив)
 //индексы       0  1  2  3  4  5  6
 int[] array = { 1, 2, 3, 4, 5, 6, 7 };
 int n = array.Length;
