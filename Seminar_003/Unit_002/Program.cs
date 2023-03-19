a685217ede26cf76546d490495e5c2ea003535b1
Funny-Bunny <work.nyu@gmail.com>
2023-03-19 23:58:46 +0300

    Add changes to Unit 002 in folder Seminar 003

---
+++ Seminar_003/Unit_002/Program.cs
@@ -0,0 +1,54 @@
+﻿// Напишите программу, которая по заднному номеру четверти показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+
+
+
+// Вариант 1 - через if/else
+Console.Clear();
+
+Console.WriteLine("Введите номер четверти: ");
+int quarter = int.Parse(Console.ReadLine()); // Parse = Convert.To, но при этом он еще может содержать литералы(пустые значения)
+
+if (quarter == 1)
+    Console.WriteLine("x > 0, y > 0");
+else if (quarter == 2)
+    Console.WriteLine("x < 0, y > 0");
+else if (quarter == 3)
+    Console.WriteLine("x > 0, y < 0");
+else if (quarter == 4)
+    Console.WriteLine("x < 0, y < 0");
+else
+    Console.WriteLine("четверть  не определена");
+
+
+
+
+// Вариант 2 - через switch
+Console.Clear();
+
+Console.WriteLine("Введите номер четверти: ");
+int quarter = int.Parse(Console.ReadLine());
+switch (quarter)
+{
+    case 1:
+        Console.WriteLine("x > 0, y > 0");
+        break;
+
+    case 2:
+        Console.WriteLine("x < 0, y > 0");
+        break;
+
+    case 3:
+        Console.WriteLine("x > 0, y < 0");
+        break;
+
+    case 4:
+        Console.WriteLine("x < 0, y < 0");
+        break;
+
+    default: Console.WriteLine("четверть  не определена");
+}
+
+
+//case 1
